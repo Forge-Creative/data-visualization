3bfb083a1b14d319e36bb76b710f7f0fb8776738
Forge-Creative <webmaster@forge.co.nz>
2023-09-08 09:45:08 +1200

    Changed share to

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import { select, range } from "d3";
+import { select, range, symbol, symbolsFill } from "d3";
 
 const link = "http://www.w3.org/2000/svg";
 const width = window.innerWidth;
@@ -21,7 +21,7 @@ svg
 	.attr("y", (d) => d * 20)
 	.attr("width", width)
 	.attr("height", 10)
-	.attr("mask", "url(#circle-mask)");
+	.attr("mask", "url(#mask-1)");
 
 // vertical lines
 svg
@@ -32,10 +32,10 @@ svg
 	.attr("x", (d) => d * 20)
 	.attr("width", 10)
 	.attr("height", height)
-	.attr("mask", "url(#circle-mask-2)");
+	.attr("mask", "url(#mask-2)");
 
 // mask
-const mask = svg.append("mask").attr("id", "circle-mask");
+const mask = svg.append("mask").attr("id", "mask-1");
 
 // maskRect
 mask
@@ -46,14 +46,14 @@ mask
 
 // circle
 mask
-	.append("circle")
-	.attr("cx", width / 2)
-	.attr("cy", height / 2)
-	.attr("r", 300)
+	.append("g")
+	.attr("transform", `translate(${width / 2}, ${height / 2})`)
+	.append("path")
+	.attr("d", symbol(symbolsFill[1], 200000)())
 	.attr("fill", "white");
 
 // mask2
-const mask2 = svg.append("mask").attr("id", "circle-mask-2");
+const mask2 = svg.append("mask").attr("id", "mask-2");
 
 // maskRect2
 mask2
@@ -64,8 +64,8 @@ mask2
 
 // circle2
 mask2
-	.append("circle")
-	.attr("cx", width / 2)
-	.attr("cy", height / 2)
-	.attr("r", 300)
+	.append("g")
+	.attr("transform", `translate(${width / 2}, ${height / 2})`)
+	.append("path")
+	.attr("d", symbol(symbolsFill[1], 200000)())
 	.attr("fill", "black");
