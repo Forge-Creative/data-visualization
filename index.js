5c6605c7a17bec24bbfdca69a90beb7bb4d6c49c
Forge-Creative <webmaster@forge.co.nz>
2023-09-08 13:10:09 +1200

    Clean up commented out codes

--- index.js
+++ index.js
@@ -8,27 +8,8 @@ const svg = select("body")
 	.attr("width", width)
 	.attr("height", height);
 
-let time = 0;
-setInterval(() => {
-	const data = range(15).map((d) => ({
-		x: d * 60 + 50,
-		y: 250 + Math.sin(d * 0.5 + time) * 200,
-		r: 20 + Math.sin(d * 0.5 + time * 2) * 10,
-	}));
-	// const circles = svg.selectAll("circle").data(data);
-
-	// const circlesEnter = circles
-	// 	.enter()
-	// 	.append("circle")
-	// 	.attr("r", 20)
-	// 	.attr("fill", "pink");
-
-	// circles
-	// 	.merge(circlesEnter)
-	// 	.attr("cx", (d) => d * 60 + 50)
-	// 	.attr("cy", (d) => 250 + Math.sin(d * 0.5 + time) * 200);
-
-	const circles = svg
+const vizData = (data) => {
+	svg
 		.selectAll("circle")
 		.data(data)
 		.join("circle")
@@ -36,6 +17,16 @@ setInterval(() => {
 		.attr("fill", "pink")
 		.attr("cx", (d) => d.x)
 		.attr("cy", (d) => d.y);
+};
 
+let time = 0;
+setInterval(() => {
+	const data = range(15).map((d) => ({
+		x: d * 60 + 50,
+		y: 250 + Math.sin(d * 0.5 + time) * 200,
+		r: 20 + Math.sin(d * 0.5 + time * 2) * 10,
+	}));
+
+	vizData(data);
 	time = time + 0.01;
 }, 1000 / 60);
