9cad7df4d8c2e1f9b89f086b2be5a76a5ad9f881
Forge-Creative <webmaster@forge.co.nz>
2023-09-08 16:12:45 +1200

    Add government period data

--- data.js
+++ data.js
@@ -1,3 +1,6 @@
+//=====
+//home ownership data
+//=====
 export const homeOwnershipRate = [
 	{ year: 1890, rate: 100 },
 	{ year: 1936, rate: 70.5 },
@@ -9,4 +12,77 @@ export const homeOwnershipRate = [
 	{ year: 1971, rate: 46.7 },
 	{ year: 1976, rate: 46.4 },
 	{ year: 1981, rate: 54.7 },
+	{ year: 1986, rate: 53.9 },
+	{ year: 1991, rate: 57.4 },
+	{ year: 1996, rate: 52.3 },
+	{ year: 2001, rate: 47 },
+	{ year: 2006, rate: 45.2 },
+	{ year: 2013, rate: 43.1 },
+	{ year: 2018, rate: 38.5 },
+	{ year: 2024, rate: 35.5 },
+	{ year: 2030, rate: 35.5 },
+];
+
+//=====
+//government period data
+//=====
+export const governments = [
+	{
+		start: { year: 1935, month: 1, day: 1 },
+		end: { year: 1949, month: 1, day: 1 },
+		government: "First Labour Government",
+		color: "red",
+		details: "Some details about First Labour Government.",
+	},
+	{
+		start: { year: 1949, month: 1, day: 1 },
+		end: { year: 1957, month: 1, day: 1 },
+		government: "First National Government",
+		color: "blue",
+		details: "Some details about First National Government.",
+	},
+	{
+		start: { year: 1957, month: 1, day: 1 },
+		end: { year: 1960, month: 1, day: 1 },
+		government: "Second Labour Government",
+		color: "red",
+		details: "Some details about Second Labour Government.",
+	},
+	{
+		start: { year: 1960, month: 1, day: 1 },
+		end: { year: 1972, month: 1, day: 1 },
+		government: "Second National Government",
+		color: "blue",
+		details: "Some details about Second National Government.",
+	},
+];
+
+//=====
+//period event data
+//=====
+export const periodEvents = [
+	{
+		start: 1935,
+		end: 1949,
+		government: "551 houses built under Native Land Development scheme",
+		color: "red",
+	},
+	{
+		start: 1949,
+		end: 1957,
+		government: "2289 houses built under Native Housing Act",
+		color: "blue",
+	},
+	{
+		start: 1957,
+		end: 1960,
+		government: "Department of Māori Affairs builds 7,312  houses",
+		color: "red",
+	},
+	{
+		start: 1960,
+		end: 1972,
+		government: "DMA and SAC build 12,903 houses for Māori",
+		color: "blue",
+	},
 ];
--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 import * as d3 from "d3";
-import { homeOwnershipRate } from "./data";
+import { homeOwnershipRate, governments, periodEvents } from "./data";
 
 const svgWidth = 1200;
-const svgHeight = 400;
+const svgHeight = 800;
 const margin = { top: 20, right: 20, bottom: 20, left: 20 };
 const width = svgWidth - margin.left - margin.right;
 const height = svgHeight - margin.top - margin.bottom;
@@ -17,9 +17,13 @@ const svg = d3
 //scale
 const xScale = d3
 	.scaleTime()
-	.domain([new Date(1840, 0, 1), new Date(new Date().getFullYear(), 0, 1)]) // up to the current year
+	.domain([new Date(1840, 0, 1), new Date(2040, 0, 1)])
+	// .domain([new Date(1840, 0, 1), new Date(new Date().getFullYear(), 0, 1)]) // up to the current year
 	.range([0, width]);
 
+//=====
+//home ownership
+//=====
 const yScale = d3
 	.scaleLinear()
 	.domain([0, 100]) //percentage
@@ -38,6 +42,64 @@ svg
 	.attr("stroke", "#7A0708")
 	.attr("stroke-width", 5)
 	.attr("fill", "none");
+
+//=====
+//government period
+//=====
+const yScaleGov = d3
+	.scaleBand()
+	.domain(governments.map((d) => d.government))
+	.range([svgHeight, svgHeight + governments.length * 30])
+	.padding(0.2);
+
+//government period bar
+const tooltip = d3.select("#tooltip");
+svg
+	.selectAll(".gov-term")
+	.data(governments)
+	.enter()
+	.append("rect")
+	.attr("class", "gov-term")
+	// .attr("y", (d) => yScaleGov(d.government))
+	.attr("y", 500)
+	.attr("x", (d) =>
+		xScale(new Date(d.start.year, d.start.month - 1, d.start.day))
+	)
+	.attr(
+		"width",
+		(d) =>
+			xScale(new Date(d.end.year, d.end.month - 1, d.end.day)) -
+			xScale(new Date(d.start.year, d.start.month - 1, d.start.day))
+	)
+	.attr("height", yScaleGov.bandwidth())
+	.attr("fill", (d) => d.color)
+	.on("mouseover", function (event, d) {
+		tooltip.transition().duration(200).style("opacity", 0.9);
+		tooltip
+			.html(
+				`<h3>${d.government}</h3>
+				${d.start.year}-${d.start.month}-${d.start.day} to ${d.end.year}-${d.end.month}-${d.end.day}<br>
+				<p> ${d.details}</p>`
+			)
+			.style("left", event.pageX + 5 + "px")
+			.style("top", event.pageY - 28 + "px");
+	})
+	.on("mouseout", function (d) {
+		tooltip.transition().duration(500).style("opacity", 0);
+	});
+
+//government label
+// svg
+// 	.selectAll(".gov-label")
+// 	.data(governments)
+// 	.enter()
+// 	.append("text")
+// 	.attr("class", "gov-label")
+// 	.attr("x", (d) => xScale(new Date(d.start, 0, 1)) + 55) // バーの少し右側
+// 	// .attr("y", (d) => yScaleGov(d.government) + yScaleGov.bandwidth() / 2)
+// 	.attr("y", 500)
+// 	.attr("dy", ".35em") // 上下中央に位置するための調整
+// 	.text((d) => d.government);
 
 //=====
 //bar
