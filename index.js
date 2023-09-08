ec3d1bdcbef46e6ecfc69e0dd2994e9c53a7ab5c
Forge-Creative <webmaster@forge.co.nz>
2023-09-08 12:55:57 +1200

    Changing circles size over time

--- index.js
+++ index.js
@@ -1,65 +1,41 @@
-import { select, range, symbol, symbolsFill } from "d3";
+import { select, range } from "d3";
 
 const width = window.innerWidth;
 const height = window.innerHeight;
 
-// svg initialization
 const svg = select("body")
 	.append("svg")
 	.attr("width", width)
 	.attr("height", height);
 
-const n = 100;
-//horizontal lines
-svg
-	.append("g")
-	.selectAll("rect")
-	.data(range(n))
-	.join("rect")
-	.attr("y", (d) => d * 20)
-	.attr("width", width)
-	.attr("height", 10)
-	.attr("mask", "url(#mask-1)");
+let time = 0;
+setInterval(() => {
+	const data = range(15).map((d) => ({
+		x: d * 60 + 50,
+		y: 250 + Math.sin(d * 0.5 + time) * 200,
+		r: 20 + Math.sin(d * 0.5 + time * 2) * 10,
+	}));
+	// const circles = svg.selectAll("circle").data(data);
 
-// vertical lines
-svg
-	.append("g")
-	.selectAll("rect")
-	.data(range(n))
-	.join("rect")
-	.attr("x", (d) => d * 20)
-	.attr("width", 10)
-	.attr("height", height)
-	.attr("mask", "url(#mask-2)");
+	// const circlesEnter = circles
+	// 	.enter()
+	// 	.append("circle")
+	// 	.attr("r", 20)
+	// 	.attr("fill", "pink");
 
-// shape mask
-const renderMask = (selection, id, inverted) => {
-	const mask = svg.append("mask").attr("id", id);
+	// circles
+	// 	.merge(circlesEnter)
+	// 	.attr("cx", (d) => d * 60 + 50)
+	// 	.attr("cy", (d) => 250 + Math.sin(d * 0.5 + time) * 200);
 
-	mask
-		.append("rect")
-		.attr("width", width)
-		.attr("height", height)
-		.attr("fill", inverted ? "black" : "white");
+	const circles = svg
+		.selectAll("circle")
+		.data(data)
+		.join("circle")
+		.attr("r", (d) => d.r)
+		.attr("fill", "pink")
+		.attr("cx", (d) => d.x)
+		.attr("cy", (d) => d.y);
 
-	// const g = mask
-	// 	.append("g")
-	// 	.attr("transform", `translate(${width / 2}, ${height / 2})`);
-
-	mask
-		.selectAll("g")
-		.data(range(symbolsFill.length))
-		.join((enter) =>
-			enter
-				.append("g")
-				.attr("transform", (d) => `translate(${d * 300}, ${height / 2})`)
-				.append("path")
-				.attr("d", (d) => symbol(symbolsFill[d], 50000)())
-				.attr("fill", inverted ? "white" : "black")
-		);
-};
-
-// renderMask(svg, "mask-1", false);
-// renderMask(svg, "mask-2", true);
-
-svg.call(renderMask, "mask-1", false).call(renderMask, "mask-2", true);
+	time = time + 0.01;
+}, 1000 / 60);
